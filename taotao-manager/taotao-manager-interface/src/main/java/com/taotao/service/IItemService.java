b7f03afb560b5dd56c866d0c8e0d7ce8bbbe818f
jieminy <jiemin.y@163.com>
2017-11-18 00:12:29 +0800

    dubbo service

---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/IItemService.java
@@ -0,0 +1,7 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+public interface IItemService {
+    TbItem getItemById(long itemId);
+}
---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemService.java
@@ -0,0 +1,19 @@
+package com.taotao.service.impl;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.IItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService implements IItemService{
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+
+        return null;
+    }
+}
